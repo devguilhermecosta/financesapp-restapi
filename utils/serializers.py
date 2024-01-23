4c4dd1776e23cf9e64b5ffb189f6e2cee1434954
guilherme costa <guilherme.partic@gmail.com>
2024-01-23 00:01:42 -0300

    update: add the user data

--- utils/serializers.py
+++ utils/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from typing import override, Dict, Any
+from utils.views import get_user_data
 
 
 class CustomTokenRefreshSerializer(TokenRefreshSerializer):
     @override
-    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
         super().validate(attrs)
         refresh = self.token_class(attrs["refresh"])
-        data = {"access": str(refresh.access_token)}
+        user_data = get_user_data(refresh)
+        data = {
+            "access": str(refresh.access_token),
+            "user": user_data,
+        }
+
         return data
--- utils/views.py
+++ utils/views.py
@@ -1,10 +1,26 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.tokens import Token
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
-from typing import override
+from typing import override, Dict
+from user.models import User
+
+
+def get_user_data(token: Token) -> Dict[str, str]:
+    """ get user info by Token """
+    auth = JWTAuthentication()
+    user: User = auth.get_user(token)  # type: ignore
+    user_data = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'full_name': user.get_full_name(),
+        'email': user.email,
+    }
+    return user_data
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -14,7 +30,14 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
-        response = Response({'access': data['access']}, status.HTTP_200_OK)
+        token = data['access']
+        user_data = get_user_data(token)
+        response = Response(
+            {
+                'access': token,
+                'user': user_data,
+            },
+            status.HTTP_200_OK)
         response.set_cookie(
             key='token_refresh',
             value=data.get('refresh'),
